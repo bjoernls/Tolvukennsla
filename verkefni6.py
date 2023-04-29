5cbf73e32e0082dd4c87e71993aa4fc7ce3075c6
Björn Sigurbergsson <76668972+bjoernls@users.noreply.github.com>
2023-04-29 12:04:10 +0000

    comment verkefni 6

--- verkefni6.py
+++ verkefni6.py
@@ -6,6 +6,7 @@ def prenta(hitastig_ar_samtals, manudir_i_ari):
 
 file = open("vedurgogn.csv", "r")
 
+#initialize
 ar = None
 hitastig_ar_samtals = 0
 manudir_i_ari = 0
@@ -13,14 +14,18 @@ manudir_i_ari = 0
 for line in file:
     gogn = line.split(',')
 
+    #sleppum að lesa fyrstu línuna
     if (gogn[0] == "ar"):
         continue
 
+    #fyrsta ítrunin
     if (ar is None):
         ar = gogn[0]
 
+    #nýtt ár byrjað í skránni, prentum út meðaltalið
     if (gogn[0] != ar):
         prenta(hitastig_ar_samtals, manudir_i_ari)
+        #endurstilla fyrir næsta ár
         ar = gogn[0]
         hitastig_ar_samtals = 0
         manudir_i_ari = 0
