46394db26c2617e489e93b93bd5bd391d3c4c7b2
bjs <bjs@elect-it.com>
2023-04-29 12:00:32 +0000

    verkefni 6

--- verkefni6.py
+++ verkefni6.py
@@ -1,33 +1,34 @@
-file = open("vedurgogn.txt", "r")
+def prenta(hitastig_ar_samtals, manudir_i_ari):
+    medalhiti = hitastig_ar_samtals / manudir_i_ari
+    print ("mánuðir árið " + ar + " eru " + str(manudir_i_ari))
+    print ("meðalhiti er " + str(medalhiti))
+    print ("\n")
 
+file = open("vedurgogn.csv", "r")
 
 ar = None
 hitastig_ar_samtals = 0
 manudir_i_ari = 0
+
 for line in file:
     gogn = line.split(',')
 
-    if (gogn[0] == "stod"):
+    if (gogn[0] == "ar"):
         continue
 
     if (ar is None):
-        ar = gogn[1]
+        ar = gogn[0]
 
-    
-    if (gogn[1] != ar):
-        
-        medaltal = hitastig_ar_samtals / manudir_i_ari
-        print ("mánuðir árið " + ar + " eru " + str(manudir_i_ari))
-        print ("meðaltal hitastigs er " + str(medaltal))
-        print ("\n")
-        ar = gogn[1]
+    if (gogn[0] != ar):
+        prenta(hitastig_ar_samtals, manudir_i_ari)
+        ar = gogn[0]
         hitastig_ar_samtals = 0
         manudir_i_ari = 0
     
-    hitastig_ar_samtals += float(gogn[3])
+    hitastig_ar_samtals += float(gogn[2])
     manudir_i_ari += 1
 
-medaltal = hitastig_ar_samtals / manudir_i_ari
-print ("mánuðir árið " + ar + " eru " + str(manudir_i_ari))
-print ("meðaltal hitastigs er " + str(medaltal))
-print ("\n")
+#prenta núverandi ár sem er ekki búið
+if (manudir_i_ari > 0):
+    prenta(hitastig_ar_samtals, manudir_i_ari)
+
