0b66359682dc51f6f6f350b7885125b47fd39a2a
bjs <bjs@elect-it.com>
2023-05-01 21:23:35 +0000

    minor breytingar á verkefni3

--- verkefni3.py
+++ verkefni3.py
@@ -1,14 +1,14 @@
 # Skrifið forrit sem fær inntak hitastig í Fahrenheit-gráðum frá notenda og skilar út Celsius-gráðum.
 
 # Fá intak frá notanda
-Tf = input("Sláðu inn hitastig í Farenheit ")
+Tf = input("Sláðu inn hitastig í Farenheit: ")
 
 # Formúlan er (f − 32) × 5/9, þar sem f er hitastig í Fahrenheit-gráðum.
-fToC = round((int(Tf) - 32) * 5/9)
+fToC = (int(Tf) - 32) * 5/9
 
 # setja breytuna fyrir celsius inn í streng til að segja hvað það er heitt úti í celcius gráðum
-txt = "Það eru {}°C úti"
-print(txt.format(fToC))
+txt = "{}°F eru {}°C"
+print(txt.format(Tf, fToC))
 
 # Prófið forritið með eftirfarandi gildum:
 # 0°F = -17.78 °C
