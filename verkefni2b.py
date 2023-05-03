8b83e1717925d7a279be6ecc463f8e0b5a549818
viktorhollanders <viktorhollanders@hey.com>
2023-05-03 16:33:17 +0000

    verkefni 2b og 2c

---
+++ verkefni2b.py
@@ -0,0 +1,6 @@
+# settu saman tvo strengi með því að nota breytur
+str1 = "Hello"
+str2 = "World!"
+
+print(str1 + str2) # verður HelloWorld!
+print(str1 + " " +str2) # verður Hello World!
---
+++ verkefni2c.py
@@ -0,0 +1,10 @@
+# margfald input með 2
+print("þetta forrit margfaldar með tveimur")
+n = input("Sláðu inn tölu: ")
+
+print(n * 2) # Þessi útgáfa gefur villu þar sem að input skilara alltaf streng
+print(int(n) * 2) # Hérna erum við búinn að nota in til að breyta streng í tölu
+
+
+
+
