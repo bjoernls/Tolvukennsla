9a867fe5101c0b6ba888d66fcc209e3399a01c6a
viktorhollanders <viktorhollanders@hey.com>
2023-05-11 22:55:23 +0000

    add control flow assignment

---
+++ controlFlow.py
@@ -0,0 +1,8 @@
+print("Check if the number is grater or smaler than 15")
+i = input("Enter a number: ")
+
+if (int(i) < 15):
+    print("{} is less than 15".format(i))
+else:
+    print("{} is grater than 15".format(i))
+    
